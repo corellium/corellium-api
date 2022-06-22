8e1a252b54b8b9fb2ea5e6bf608ad37c333db181
jasmine <jasmine@corellium.com>
2022-06-22 08:01:15 +0000

    MD-1169: Added device creation/demo example.

---
+++ examples/device_example/main.js
@@ -0,0 +1,110 @@
+const { Corellium } = require("@corellium/corellium-api");
+
+async function main() {
+    // ACCOUNT CREDENTIALS
+    let myEndPoint = "https://app.corellium.com";
+    let myUserName = "user@name.foo";
+    let myPassword = "<password>";
+    let myProject = "Example Project Name";
+
+    // CREDENTIALS/CONFIGURATIONS
+    let myDeviceName = "API Android";
+    let myFlavor = "ranchu";
+    let myOS = "11.0.0";
+    let myFile = "devicetree";
+    let myFilePath = "./devicetree";
+
+    // Sets up a new Corellium endpoint to use
+    let corellium = new Corellium({
+        endpoint: myEndPoint,
+        username: myUserName,
+        password: myPassword,
+    });
+
+    // Login.
+    console.log("[+] Logging in...");
+    await corellium.login();
+
+    // Get the list of projects.
+    let project;
+    let projects = await corellium.projects().catch((e) => console.log(e));
+
+    if (projects) {
+        // Find the project called myProject.
+        console.log("[+] Getting the project named " + myProject);
+        project = await projects.find((project) => project.name === myProject);
+        if (!project) {
+            console.log("[!] There is no project named " + myProject);
+            return;
+        } else {
+            console.log("[+] Found the " + myProject + " project.");
+        }
+    } else {
+        console.log("[!] You don't have any projects.");
+        return;
+    }
+
+    // Get the instances in the project.
+    console.log("[+] Getting instances...");
+    let instances = await project.instances();
+
+    // Send the list of instances to the console
+    for (let i = 0; i < instances.length; i++) {
+        console.log("[+] Found instance named " + instances[i].info.name);
+    }
+
+    // Find the instance
+    let instance = instances.find((instance) => instance.name === myDeviceName);
+
+    if (!instance) {
+        // Create the device
+        console.log(
+            "[!] " +
+                myDeviceName +
+                " device not found. \n[+] Creating a " +
+                myFlavor +
+                " device...",
+        );
+        instance = await project.createInstance({
+            flavor: myFlavor,
+            os: myOS,
+            name: myDeviceName,
+        });
+    } else {
+        if (instance.state === "off") {
+            console.log("[+] Turning on the device...");
+            await instance.start();
+        } else {
+            console.log("[+] Rebooting the device...");
+            await instance.reboot();
+        }
+    }
+    await instance.waitForState("on");
+
+    // Check how many snapshots are already on this instance
+    let snapshots = await instance.snapshots();
+    if (snapshots.length < 5) {
+        console.log("[+] Turning off the device...");
+        await instance.stop();
+        await instance.waitForTaskState("none");
+
+        console.log("[+] Taking a snapshot...");
+        await instance.takeSnapshot("TestSnapshot");
+        await instance.waitForTaskState("none");
+    } else {
+        console.log("[!] Can't have more than 5 snapshots.");
+    }
+
+    // Upload the custom devicetree
+    console.log("[+] Uploading custom devicetree...");
+    await instance.uploadDeviceTree(myFilePath, myFile);
+
+    await instance.waitForTaskState("none");
+
+    console.log("[+] Done!");
+    return;
+}
+
+main().catch((err) => {
+    console.error(err);
+});
