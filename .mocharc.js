56cb917b4c37376d75bc7f00b3a456a701317943
judy <judy@corellium.com>
2023-08-07 14:57:37 +0000

    Fix(corellium): CORE-3112 - Update getToken to use stored promise from memory

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,5 @@
 
 module.exports = {
   require: ['test/root-hook-set-flag-if-test-failed.js'],
-  exclude: ["**/integration-tests.js"]
+  exclude: ["**/integration-tests.js", "**/corellium-tests.js"]
 };
